a9fff73173e4461d6803aa5b45ac01b68d2a87b9
Elsie Oduor <151344037+yukiodesigns@users.noreply.github.com>
2024-04-24 14:19:43 +0300

    Added flight route

--- routes/flight.js
+++ routes/flight.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {bookFlight, getFlight, confirmAttendance, confirmFlight} = require('../controllers/flight')
+const {bookFlight, getFlight, confirmAttendance, confirmFlight, paidFlight} = require('../controllers/flight')
 
 router.post('/book', authMiddleware, bookFlight)
 router.get('/', getFlight)
 router.post('/confirm-attendance', authMiddleware, confirmAttendance)
 router.post('/confirm-flight', authMiddleware, confirmFlight)
+router('/pay', authMiddleware, paidFlight)
 
 module.exports = router
