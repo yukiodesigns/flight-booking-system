9d30edf2a9bdbaea37dc7f91c59acccb7cc58910
Elsie Oduor <151344037+yukiodesigns@users.noreply.github.com>
2024-04-24 15:10:42 +0300

    Finishing touches

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const auth = require('./routes/auth')
+const flight = require('./routes/flight')
+
+const app = express()
+
+mongoose.connect('mongodb://localhost:27017/FlightBookingSystem').then(()=>{ 
+    console.log('Connected to Mongodb')
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000')
+    })
+}).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+})
